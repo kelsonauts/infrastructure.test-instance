b8f7408c49543f54f96974230428b5363ab356ef
Aleksandr Lopatkin <aleksandr_lopatkin@epam.com>
2018-10-07 23:29:50 +0300

    Resources to run test instance and associate EIP to it

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+
+STACK_NAME="TestingEC2"
+TEMPLATE_NAME="testing-instance.yaml"
+
+if [[ `aws cloudformation describe-stacks | jq ".Stacks[] | select(.StackName == \"${STACK_NAME}\")"` ]]; then 
+	aws cloudformation update-stack --stack-name "${STACK_NAME}"  \
+		--template-body file://"${TEMPLATE_NAME}" \
+		--parameters file://parameters.json
+
+	# aws cloudformation wait stack-update-complete --stack-name "${STACK_NAME}"
+else
+	aws cloudformation create-stack --stack-name "${STACK_NAME}"  \
+		--template-body file://"${TEMPLATE_NAME}" \
+		--parameters file://parameters.json
+
+	# aws cloudformation wait stack-create-complete --stack-name "${STACK_NAME}"
+fi
